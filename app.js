1eca19619a0e2be5e423318536cff56b267b97cd
Chris <orj.tsuk@gmail.com>
2022-11-30 08:36:42 -0500

    Setting up basic routes.

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
-let dotenv = require('dotenv')
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 
 // Initialize dotenv
 dotenv.config()
 
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
 
 // Set up MongoDB
 const mongoDB = process.env.MONGO
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
