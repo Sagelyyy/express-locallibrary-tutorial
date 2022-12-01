caa5c28682efb4dfed7fac772e366d2692fb986e
Chris <orj.tsuk@gmail.com>
2022-12-01 08:48:11 -0500

    filled out the controllers for getting the lists, added views for each corresponding controller list.

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -28,6 +29,15 @@ AuthorSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`;
 });
+
+// Make dates looks pretty
+AuthorSchema.virtual("birth_date_formatted").get(function(){
+  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+})
+
+AuthorSchema.virtual("death_date_formatted").get(function(){
+  return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+})
 
 // Export model
 module.exports = mongoose.model("Author", AuthorSchema);
